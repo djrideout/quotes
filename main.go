f40dfa0873f9fed90b91d60d4836668d79469572
D.J. Rideout <djrideoutgit@proton.me>
2024-10-28 00:40:25 +0000

    Use command line flags

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"math/rand"
@@ -9,15 +10,14 @@ import (
 	"os"
 )
 
+var fileName = flag.String("input", "quotes.txt", "The file to read lines from")
+var port = flag.Int("port", 8080, "The port to serve from")
+
 func main() {
-	args := os.Args[1:]
-	port := "8080"
-	if len(args) > 0 {
-		port = args[0]
-	}
+	flag.Parse()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		file, err := os.Open("quotes.txt")
+		file, err := os.Open(*fileName)
 		if err != nil {
 			fmt.Println("Error opening file:", err)
 			return
@@ -39,5 +39,5 @@ func main() {
 		fmt.Fprint(w, line)
 	})
 
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
