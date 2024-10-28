b426340f9898fb26cdcda09c49fb43d34325712f
D.J. Rideout <djrideoutgit@proton.me>
2024-10-28 02:14:17 +0000

    Add quotes with a POST request

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"math/rand"
 	"net/http"
@@ -17,26 +18,42 @@ func main() {
 	flag.Parse()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		file, err := os.Open(*fileName)
+		file, err := os.OpenFile(*fileName, os.O_RDWR|os.O_APPEND, 666)
 		if err != nil {
 			fmt.Println("Error opening file:", err)
 			return
 		}
 		defer file.Close()
 
-		var lines []string
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			lines = append(lines, scanner.Text())
-		}
+		if r.Method == "POST" {
+			line, err := io.ReadAll(r.Body)
+			if err != nil {
+				fmt.Println("Error reading request body:", err)
+				return
+			}
+			defer r.Body.Close()
 
-		if err := scanner.Err(); err != nil {
-			fmt.Println("Error reading file:", err)
-			return
-		}
+			lineStr := string(line)
+			fmt.Fprintf(file, "\n%s", lineStr)
+			fmt.Fprintf(w, "Added \"%s\"", lineStr)
+		} else {
+			var lines []string
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				lines = append(lines, scanner.Text())
+			}
 
-		line := lines[rand.Intn(len(lines))]
-		fmt.Fprint(w, line)
+			if err := scanner.Err(); err != nil {
+				fmt.Println("Error reading file:", err)
+				return
+			}
+
+			line := ""
+			if len(lines) > 0 {
+				line = lines[rand.Intn(len(lines))]
+			}
+			fmt.Fprint(w, line)
+		}
 	})
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
